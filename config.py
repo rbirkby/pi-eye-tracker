4aafc6da32755969ded75043b57a8dfe47d6fc3c
Richard Birkby <rbirkby@gmail.com>
2017-10-22 09:16:02 +0000

    Initial Commit

---
+++ config.py
@@ -0,0 +1,21 @@
+
+# Display Settings
+# ----------------
+debug = True        # Set to False for no data display
+#show_fps = False    # Show Frames per second
+
+# Camera Settings
+# ---------------
+CAMERA_WIDTH = 320    # default = 320 PiCamera image width can be greater if quad core RPI
+CAMERA_HEIGHT = 240   # default = 240 PiCamera image height
+CAMERA_HFLIP = False  # True=flip camera image horizontally
+CAMERA_VFLIP = False  # True=flip camera image vertically
+CAMERA_ROTATION = 0   # Rotate camera image valid values 0, 90, 180, 270
+CAMERA_FRAMERATE = 25 # default = 25 lower for USB Web Cam. Try different settings
+#FRAME_COUNTER = 1000  # used when show_fps=True  Sets frequency of display
+
+# Motion Track Settings
+# ---------------------
+MIN_AREA = 200       # excludes all contours less than or equal to this Area
+THRESHOLD_SENSITIVITY = 25
+BLUR_SIZE = 10
